e6d6b42ddd58e357a6a1b6ab46eaaaa71503015f
Istip <isticsek@gmail.com>
2022-02-27 19:03:17 +0200

    get tickets redux

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -15,6 +15,19 @@ const createTicket = async (ticketData, token) => {
   return response.data;
 };
 
-const ticketService = { createTicket };
+// Get users tickets
+const getTickets = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+const ticketService = { createTicket, getTickets };
 
 export default ticketService;
